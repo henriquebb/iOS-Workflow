b21fbbe208972f90ab657a1199733c26e6e9f73f
Henrique Barbosa <henriqbarbosa@icloud.com>
2025-08-09 15:06:54 -0300

    Initial Commit

---
+++ iOS Workflow/iOS_WorkflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_WorkflowApp.swift
+//  iOS Workflow
+//
+//  Created by Henrique Batista Barbosa on 09/08/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_WorkflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
